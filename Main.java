050e93f46db74b198e2bbac9924f7e57e1a7f2d9
Student: Ranim Salame <112954177+ranimsalame@users.noreply.github.com>
2022-09-06 13:22:37 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,42 @@
+import java.util.*; 
+class Main {
+  public static void main(String[] args) {
+    Scanner it=new Scanner (System.in);
+
+//Q1 how do you use the scanner to populate the following variables
+//System.out.println("Please enter your first name: ");
+//String x=it.nextLine();
+//System.out.println("Hi "+x+"!");
+//System.out.println("Please enter your age: ");
+//int age=it.nextInt();
+//System.out.println("Please enter your height in cm: ");
+//double height=it.nextDouble();
+//System.out.println("You are "+age+" years old and are "+height+" cm tall!");
+
+//int num=723;
+//Q2 how do you print the hundreds digit
+//System.out.println("hundreds digit="+num/100);
+//how do you print the ones digit
+//System.out.println("ones digit="+num%10);
+//how do you print the tens digit of the original number
+//System.out.println("ten's digit="+(num/10)%10);
+
+//Q3 what value is output when val is printed
+//int val=-123;
+  //val--;
+  //val--;
+  //val--;
+  //System.out.println(val);
+
+//Q4 correct the code
+  //int sum=(int)-123.456;
+
+//Q5 How do you store the phrase Have a nice day in a String variable called greeting
+//String greeting = "Have a nice day";
+    //System.out.println(greeting);
+//What is the output of the following lines of code
+    System.out.println(100+100);
+    System.out.println("s"+100+100);
+    System.out.println(100+(100+100));
+  }
+}
